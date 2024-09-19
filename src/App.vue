b0a5b29a0d5d45841a0f41ab8520a5f66cc2952f
matthewmillerh <matthewmillerh@outlook.com>
2024-09-19 08:32:25 +0200

    maintenance branch

--- src/App.vue
+++ src/App.vue
@@ -65,109 +65,10 @@ function toggleView(){
 </script>
 
 <template>
-  <div v-if="loggedIn" class="wrapper pl-3 pr-3"> <!-- Password wrapper -->
-    <header>
-    <div class="flex max-w-3xl ml-auto mr-auto mt-6 bg-gray-200 rounded-lg align-middle">
-      <div class="p-5">
-        <span class="text-4xl font-bold w-auto">ITINERARY</span>
-        <p class="text-sm">24 December, 2024 - 22 January, 2025</p>
-      </div>
-      <div class="p-5 ml-auto order-2">   
-        <p>Emily Rae Smith Labuschagne</p>  
-        <div class="border-b-2 border-gray-300 mt-1"></div>  
-        <p>Matthew Jason Miller</p>      
-      </div>
-    </div>
-  </header>
 
-  <!-- Content filter bar -->
-  <div class="max-w-3xl rounded-lg bg-gray-200 ml-auto mr-auto min-h-12 p-2 mt-3 flex items-center z-20 relative">
-    <!-- Countries filter -->
-    <div class="min-h-7 flex items-center">
-      <button class="text-sm min-h-7 pl-3 pr-3 bg-white rounded-3xl font-semibold hover:bg-gray-400 transition-colors" @click="filterCountries">
-        <i class="fa-solid fa-earth-europe" v-if="showCountries === false"></i>
-        <i class="fa-solid fa-circle-xmark" v-else></i>
-        &nbsp;<span>{{ countryFilterBtn }}</span>
-      </button>
-    </div>
-    <!-- Show Files filter -->
-    <div class="text-sm font-semibold min-h-7 flex items-center ml-2 rounded-3xl bg-white pl-3 pr-3">
-      Show Files<input class="ml-1" type="checkbox" v-model="showFiles"/>
-    </div>
-    <!-- Show costs filter -->
-    <div class="text-sm font-semibold min-h-7 flex items-center ml-2 mr-2 rounded-3xl bg-white pl-3 pr-3">
-      Show Costs<input class="ml-1" type="checkbox" v-model="showCost"/>
-    </div>
-    <!-- Map view button -->
-    <div class="min-h-7 flex items-center ml-auto order-2">
-      <button class="text-sm p-1 bg-white rounded-lg font-semibold hover:bg-gray-400 transition-colors" @click="toggleView">
-        <i class="fa-solid fa-globe" v-if="!mapView"></i>
-        <i class="fa-solid fa-file-lines" v-else></i>
-        &nbsp;{{ viewBtnText }}
-      </button>
-    </div>
-    
-  </div>
-  <!-- Countries filter list -->   
-  <div class="max-w-3xl min-h-10 bg-gray-300 -mt-3 rounded-xl ml-auto mr-auto z-0 relative transition-all" v-if="showCountries">
-    <div class="flex pt-4 ml-3 items-center flex-wrap">
-      <div v-for="country in countries" class="flex items-center">
-        <span class="mr-1 text-sm">{{ country }}</span> <input type="checkbox" :id="country" :value="country" class="mr-2" v-model="filteredCountriesList">
-    </div>
-    </div>
-    
-  </div>
-
-<!-- Text view wrapper -->
-  <div v-if="!mapView">
-    <!-- Loop through each day in the itinerary -->
-      <!--Day Start-->
-      <div v-for="day in data.day">
-        <div v-if="filterCountriesList(day.countries)">
-          <Itinerary>
-            <template v-slot:date>
-              <span v-html="day.date"></span>
-            </template>  
-            <template v-slot:countries>
-              <span v-html="day.countries"></span>
-            </template>  
-          </Itinerary>
-          <!-- loop through each item in the day -->
-          <div v-for="item in day.items">
-            <ItineraryItem :show-files="showFiles" :file-url="item.file" :show-cost="showCost">
-              <template v-slot:icon>
-                <i v-if="item.type === 'Flight'" class="fa-solid fa-plane"></i>
-                <i v-if="item.type === 'Train'" class="fa-solid fa-train"></i>
-                <i v-if="item.type === 'Hotel'" class="fa-solid fa-hotel"></i>
-                <i v-if="item.type === 'Bus'" class="fa-solid fa-bus"></i>
-                <i v-if="item.type === ''" class="fa-regular fa-circle-dot"></i>
-              </template>  
-              <template #details>
-                <span v-html="item.details"></span>
-              </template>
-              <template #cost>
-                {{ item.cost.toLocaleString('en-US', { style: 'currency', currency: 'ZAR' }) }}
-              </template>
-            </ItineraryItem>
-          </div>
-        </div>
-          
-      </div>
-      <!--Day End-->
-  </div><!-- End Text view wrapper -->
-
-  <!-- Map View wrapper -->
-  <div v-else class="max-w-3xl mr-auto ml-auto mt-5">
-    <div id="layout" class="max-w-full">
-      <Map />
-    </div>
-  </div><!-- End Map View wrapper -->
-  
-  
-  </div> <!-- End Password Wrapper-->
 
   <!-- Login Div -->
-  <div v-else>
+  <div>
     <div class="h-40 w-80 bg-slate-200 rounded-lg p-5 absolute top-0 bottom-0 left-0 right-0 m-auto">
       <div class="text-center">
         <p class="mb-5 font-semibold">Enter Password to Continue</p>
@@ -176,7 +77,6 @@ function toggleView(){
       </div>
       
     </div>
-    
   </div>
 
 </template>
